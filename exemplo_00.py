7982ac11793982e6f93d1373d985380ea6217baa
Rafael Corte <rafcorte@gmail.com>
2026-02-05 09:40:28 -0300

    validação de dados com pydantic

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,10 +1,24 @@
 import requests
+from pydantic import BaseModel
 
-response = requests.get(f'https://pokeapi.co/api/v2/pokemon/15')
-data = response.json()
-data_types = data['types']
-types_list = []
-for type_info in data_types:
-    types_list.append(type_info['type']['name'])
-types = ', '.join(types_list)
-print(data['name'], types)
+class PokemonSchema(BaseModel):
+    name: str
+    type: str
+    
+    class ConfigDict:
+        from_attributes = True
+
+def pegar_pokemon(id: int) -> PokemonSchema:
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}')
+    data = response.json()
+    data_types = data['types']
+    types_list = []
+    for type_info in data_types:
+        types_list.append(type_info['type']['name'])
+    types = ', '.join(types_list)
+    return PokemonSchema(name=data['name'], type=types)
+
+if __name__ == '__main__':
+    print(pegar_pokemon(10))
+    print(pegar_pokemon(6))
+    print(pegar_pokemon(25))

8d281331276c6fd0798034868cf27789a9fa6d0a
Rafael Corte <rafcorte@gmail.com>
2026-02-05 10:45:19 -0300

    controlado

--- src/db.py
+++ src/db.py
@@ -5,4 +5,4 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./pokemon.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base
+Base = declarative_base()
